12efde8c561cf738a68fa912fdff77518347554e
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-03 01:24:08 +0400

    Add class for generating string for db

---
+++ database/connect_db.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
+import asyncio
+from utils.settings import Settings
+import logging
+
---
+++ database/db_settings.py
@@ -0,0 +1,16 @@
+from string import Template
+
+from utils.settings import Settings
+
+class DBSettings:
+     CONNECT_STRING = Template("postgresql+asyncpg://${user}:${password}@${hostname}/${dbname}")
+     
+     @classmethod
+     def get_url(cls) -> str:
+         return cls.CONNECT_STRING.substitute(
+             user = Settings.get_sql_user(),
+             password = Settings.get_sql_password(),
+             hostname = Settings.get_sql_host(),
+             dbname = Settings.get_sql_db_name()
+         )
+         
