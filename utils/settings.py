6b44ba2cb483a1f41bc47c119f6ad626488da9b7
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-03 01:17:30 +0400

    add settings for bd

--- utils/settings.py
+++ utils/settings.py
@@ -3,27 +3,56 @@ import logging
 
 from dotenv import load_dotenv
 
+
 class Settings:
-    """Class for reading setting from envieroment
-    """
-    TOKEN = "BOT_TOKEN"
-    ADMIN = 'ADMIN_ID'
+    """Class for reading setting from envieroment"""
+
+    ADMIN = None
+    TOKEN = None
+    _TOKEN = "BOT_TOKEN"
+    _ADMIN = "ADMIN_ID"
+    _SQL_USER = "SQL_USER"
+    _SQL_PASSWORD = "SQL_PASSWORD"
+    _SQL_HOST = "SQL_HOSTNAME"
+    _SQL_DB_NAME = "SQL_DATABASE_NAME"
+
     @classmethod
     def load_data_from_file(cls):
-        """tries to read data from file into environment
-        """
-        dotenv_path = os.path.join(os.getcwd(), '.env')
+        """tries to read data from file into environment"""
+        dotenv_path = os.path.join(os.getcwd(), ".env")
         if os.path.exists(dotenv_path):
             load_dotenv(dotenv_path)
             logging.info("Data from ebv has been uploaded to environment")
         else:
-            logging.warning("Script didn't find .env file to upload file to environment")
-            
+            logging.warning(
+                "Script didn't find .env file to upload file to environment"
+            )
+
     @classmethod
     def get_token(cls):
-        return os.getenv(cls.TOKEN)
+        return os.getenv(cls._TOKEN)
+
     @classmethod
     def get_admin_id(cls):
-        return os.getenv(cls.ADMIN)
-            
-            
+        return os.getenv(cls._ADMIN)
+
+    @classmethod
+    def init_variables(cls):
+        cls.ADMIN = cls.get_admin_id()
+        cls.TOKEN = cls.get_token()
+
+    @classmethod
+    def get_sql_user(cls):
+        return os.getenv(cls._SQL_USER)
+
+    @classmethod
+    def get_sql_password(cls):
+        return os.getenv(cls._SQL_PASSWORD)
+    
+    @classmethod
+    def get_sql_host(cls):
+        return os.getenv(cls._SQL_HOST)
+    
+    @classmethod
+    def get_sql_db_name(cls):
+        return os.getenv(cls._SQL_DB_NAME)
